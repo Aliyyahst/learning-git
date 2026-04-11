8c26cfaa475620075cce7b1482fa503cf3db999e
Salman Aliyyah <salmanaliyah2@gmail.com>
2026-04-11 13:50:38 +0100

    add some js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ➜  learning-git git:(feature-xyz) cat index.js
 <!DOCTYPE html>
 <html>
@@ -8,3 +9,17 @@
 <h1> first paragraph 
 </body>
 </html>
+=======
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
+>>>>>>> 3f23d00 (add some js)
